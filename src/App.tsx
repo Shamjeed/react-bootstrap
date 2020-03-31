52952c4ae6a70feb5a3f545714a086d7a1aa97f5
shamjeed <shamjeedtc@gmail.com>
2020-03-31 21:09:09 +0400

    Added menu links

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,16 @@ import 'react-bootstrap';
 
 function App() {
     return (
-        <div className="container bg-secondary text-light">
-            Hello World!!!
+        <div>
+            <nav className="navbar navbar-expand-md">
+                <ul className="navbar-nav">
+                    <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+                    <li className="nav-item"><a className="nav-link" href="#">Skills</a></li>
+                    <li className="nav-item"><a className="nav-link" href="#">Projects</a></li>
+                    <li className="nav-item"><a className="nav-link" href="#">Team</a></li>
+                    <li className="nav-item"><a className="nav-link" href="#">Contact</a></li>
+                </ul>
+            </nav>
         </div>
     );
 }
