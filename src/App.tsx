32584d2193688581abc6f1e03906585e24250626
shamjeed <shamjeedtc@gmail.com>
2020-03-31 21:32:06 +0400

    Added menu dark background

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,14 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 function App() {
     return (
         <div>
-            <nav className="navbar navbar-expand-md navbar-light">
+            <nav className="navbar navbar-expand-md navbar-light bg-dark">
                 <a href="#"><FontAwesomeIcon className="text-warning fa-2x" icon={faChild}></FontAwesomeIcon></a>
                 <ul className="navbar-nav">
-                    <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#">Skills</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#">Projects</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#">Team</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#">Contact</a></li>
+                    <li className="nav-item"><a className="nav-link text-light" href="#">Home</a></li>
+                    <li className="nav-item"><a className="nav-link text-light" href="#">Skills</a></li>
+                    <li className="nav-item"><a className="nav-link text-light" href="#">Projects</a></li>
+                    <li className="nav-item"><a className="nav-link text-light" href="#">Team</a></li>
+                    <li className="nav-item"><a className="nav-link text-light" href="#">Contact</a></li>
                 </ul>
             </nav>
         </div>
