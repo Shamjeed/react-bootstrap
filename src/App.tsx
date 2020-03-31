559948fbea2388a71f99e7596f4b01c2efc3b661
shamjeed <shamjeedtc@gmail.com>
2020-03-31 21:51:19 +0400

    Added menu updates

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,19 @@ function App() {
             <nav className="navbar navbar-expand-md navbar-light bg-dark">
                 <a href="#"><FontAwesomeIcon className="text-warning fa-2x" icon={faChild}></FontAwesomeIcon></a>
                 <ul className="navbar-nav">
-                    <li className="nav-item"><a className="nav-link text-light" href="#">Home</a></li>
-                    <li className="nav-item"><a className="nav-link text-light" href="#">Skills</a></li>
-                    <li className="nav-item"><a className="nav-link text-light" href="#">Projects</a></li>
-                    <li className="nav-item"><a className="nav-link text-light" href="#">Team</a></li>
-                    <li className="nav-item"><a className="nav-link text-light" href="#">Contact</a></li>
+                    <li className="nav-item"><a className="nav-link text-light text-uppercase font-weight-bold px-3" href="#">Home</a></li>
+                    <li className="nav-item"><a className="nav-link text-light text-uppercase font-weight-bold px-3" href="#">Skills</a></li>
+                    <li className="nav-item dropdown"><a data-toggle="dropdown" className="nav-link text-light text-uppercase font-weight-bold px-3" href="#">Projects</a>
+                        <div className="dropdown-menu">
+                            <a className="dropdown-item" href="#">Project 1</a>
+                            <a className="dropdown-item" href="#">Project 2</a>
+                            <a className="dropdown-item" href="#">Project 3</a>
+                            <a className="dropdown-item" href="#">Project 4</a>
+                            <a className="dropdown-item" href="#">Project 5</a>
+                        </div>
+                    </li>
+                    <li className="nav-item"><a className="nav-link text-light text-uppercase font-weight-bold px-3" href="#">Team</a></li>
+                    <li className="nav-item"><a className="nav-link text-light text-uppercase font-weight-bold px-3" href="#">Contact</a></li>
                 </ul>
             </nav>
         </div>
